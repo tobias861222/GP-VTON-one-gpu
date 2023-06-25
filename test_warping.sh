6fc83f1784ab995b9bd8b4d262c59db686ffd135
tobias861222 <96098258+tobias861222@users.noreply.github.com>
2023-06-26 00:56:21 +0800

    Update test_warping.sh

--- test_warping.sh
+++ test_warping.sh
@@ -1,18 +1,3 @@
-#python -m torch.distributed.launch --master_port=4739 \
-#test_warping.py \
-#    --name test_partflow_vitonhd_unpaired_1109 \
-#    --PBAFN_warp_checkpoint './checkpoints/gp-vton_partflow_vitonhd_usepreservemask_lrarms_1027/PBAFN_warp_epoch_121.pth' \
-#    --resize_or_crop None --verbose --tf_log \
-#    --batchSize 2 --label_nc 14 --launcher pytorch \
-#    --image_pairs_txt test_pairs_unpaired_1018.txt \
-#	--dataroot "./dataset/VITON-HD" \
-#	--gpu_ids 0 \
-	
-#!/bin/sh
-### for vitonhd
-### warping
-
-
 python test_warping.py \
     --name test_partflow_vitonhd_unpaired_1109 \
     --PBAFN_warp_checkpoint './checkpoints/gp-vton_partflow_vitonhd_usepreservemask_lrarms_1027/PBAFN_warp_epoch_121.pth' \
